ce2b9e210ba727fa227aa73371489cbb8560476b
Jessica Nelson <yahwehschica@yahoo.com>
2022-02-22 12:04:56 -0500

    Assignment Complete

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} genres={genresArr} posterSrc={posterURL} />
     </div>
   );
 }
